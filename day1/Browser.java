e1226e65c2601e2269c8848c7d38b5c85cb3fef3
Dhivya1821 <147966961+Dhivya1821@users.noreply.github.com>
2023-10-15 11:43:09 +0530

    Add files via upload
    day1

---
+++ day1/Browser.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Browser {
+	public static void main(String[] args) {
+		 float browserVersion = 1123.2f;
+		 String browserName = "firefox";
+		 boolean  pageLoaded = true;
+		 int  noOfBrowser = 5;
+		 char browserLogo = 'D';
+		 System.out.println(browserVersion+" " +browserName+" "+ pageLoaded+ " "+ noOfBrowser+" "+browserLogo);
+		 
+		}
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int b = 1;
+		
+		if(b>0) {
+			System.out.println("The number is positive");	
+		}
+		else {
+			System.out.println("The number is negative");
+		}
+	}
+
+}
